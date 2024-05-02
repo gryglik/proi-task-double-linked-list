eeb8e974147336ebf784249611a3e0a4db4932a8
Michał Gryglicki <michal.gryglicki2.stud@pw.edu.pl>
2024-05-02 21:17:14 +0200

    declared postincrementation operator to iterator

--- dllist.cpp
+++ dllist.cpp
@@ -1,1 +1,8 @@
 #include "dllist.h"
+
+template<typename T> dllist<T>::iterator dllist<T>::iterator::operator++(int)
+{
+    iterator old_it = *this;
+    this->pntr = this->pntr->next;
+    return old_it;
+};
